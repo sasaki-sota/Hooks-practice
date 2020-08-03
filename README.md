1b9e0f3887e83e9d78a26220afcac9293fd4c6d7
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-08-03 18:35:37 +0900

    useState

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Hooks-practice
+# useStateについて
+**基本的な記述の仕方**  
+`const [名前, set名前] = useState(初期の値)`  
+↑例として  
+`const [hoge, setHoge] = useState(0)`  
+**動きとして**  
+    hoge = 0  
+    setHoge = 関数(function)  
+となる
+
+## 使用例
+**1足したいとき**  
+`const increment = () => setHoge(hoge + 1)`  
+と記述するのが一般的である(クラスコンポーネントのsetstateのような書き方)。  
+また中に関数を記述することも可能  
+`const increment = () => setHoge(previousHoge => previousHoge + 1)`  
+previousHoge = げんざいのhogeの状態  
+と表す。
--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const App =() => {
+
+    //0が初期値となる
+    const [count, setCount] = useState(0)
+
+    // countで0 setCountで関数が返ってくる
+    // console.log({count});
+    // console.log({setCount});
+
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+
+    const increment2 = () => setCount(previousCount => previousCount + 1)
+    const decrement2 = () => setCount(previousCount => previousCount - 1)
+    const reset = () => setCount(0);
+
+    const double = () => setCount(count * 2);
+    const half3 = () => setCount(previousCount => {
+        return previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    });
+
   return (
-    <div>
-        this is a template for react app
-    </div>
+      <React.Fragment>
+          <div>count: {count}</div>
+          <button onClick={increment}>+1</button>
+          <button onClick={decrement}>-1</button>
+          <div>
+              <button onClick={increment2}>+1</button>
+              <button onClick={decrement2}>-1</button>
+          </div>
+          <div>
+              <button onClick={reset}>reset</button>
+          </div>
+          <div>
+              <button onClick={double}>x2</button>
+              <button onClick={half3}>3のときだけ割る</button>
+          </div>
+      </React.Fragment>
   );
 }
 
